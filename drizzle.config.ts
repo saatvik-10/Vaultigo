7a7a207f4612d852e8b5d73c82a2573cd05d1fb5
saatvik-10 <saatvikmadan529@gmail.com>
2025-05-01 13:56:20 +0530

    created migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     url: process.env.DATABASE_URL!,
   },
   migrations: {
-    table: '__drizzle_migartion',
+    table: '__drizzle_migrations',
     schema: 'public',
   },
   verbose: true, //detailed logs and output while running push, generate, etc.
