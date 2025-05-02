c2c55231c91122cdf87ae7c027dbfc16f9daf60b
saatvik-10 <saatvikmadan529@gmail.com>
2025-05-03 00:56:25 +0530

    added files GET api

---
+++ app/api/files/route.ts
@@ -0,0 +1,44 @@
+import { auth } from '@clerk/nextjs/server';
+import { files } from '@/lib/db/schema';
+import { db } from '@/lib/db';
+import { eq, isNull, and } from 'drizzle-orm';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json('Unauthorized', { status: 401 });
+    }
+
+    const searchParams = req.nextUrl.searchParams;
+    const queryUserId = searchParams.get('id');
+    const parentId = searchParams.get('parentId');
+
+    if (!queryUserId || queryUserId !== userId) {
+      return NextResponse.json('Bad Request', { status: 400 });
+    }
+
+    //fetch files from the db
+    let userFiles;
+
+    if (parentId) {
+      //fetch from a specific folder
+      userFiles = await db
+        .select()
+        .from(files)
+        .where(and(eq(files.userId, userId), eq(files.parentId, parentId)));
+    } else {
+      //fetch from root folder
+      userFiles = await db
+        .select()
+        .from(files)
+        .where(and(eq(files.userId, userId), isNull(files.parentId)));
+    }
+
+    return NextResponse.json(userFiles);
+  } catch (err) {
+    console.error('Error fetching files:', err);
+    return NextResponse.json('Internal Server Error', { status: 500 });
+  }
+}
